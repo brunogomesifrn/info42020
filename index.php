72976036cc18600d66586bfe988d8ebb2b29d5b4
brunogomesifrn <brunogomesifrn@gmail.com>
2021-03-08 18:59:03 -0300

    Finalizado etapa 01

--- index.php
+++ index.php
@@ -10,8 +10,42 @@
     <h1>Página Inicial</h1>
     <a href="login.html">Login</a>
 
-    <h2>Produtos Cadastrados:</h2>
     
+    <!--<h2>Produtos Cadastrados:</h2>-->
+    <?php 
+        /*
+        include 'banco.php';
+        $conn = conectar();
+
+        $sql = "SELECT * FROM Produtos order by nome";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "Produto: ".$row["nome"]."<br />";
+
+                $sql2 = "SELECT nome FROM Fornecedores where id=".$row["id_fornecedores"];
+                $result2 = mysqli_query($conn, $sql2);
+
+                while($row2 = mysqli_fetch_assoc($result2)) {
+                    echo "Fornecedor: ".$row2["nome"]."<br />";
+                }
+
+                echo "Categorias: ";
+                
+
+
+                
+                
+                echo "<br />";
+            }
+            desconectar($conn);
+
+        } else {
+            desconectar($conn);
+            echo "Nenhum produto cadastrado";
+        }*/
+    ?>
 
     <!-- Lista de Produtos -->
 </body>
